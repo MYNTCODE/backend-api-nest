64c94c34750bc6452ed5fe8654150cec1cdb7386
myntp <119732035+MYNTCODE@users.noreply.github.com>
2023-12-01 17:57:42 +0700

    feat: connect database (MongoDB)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
